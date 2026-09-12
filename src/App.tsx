dce82a306a8b2e203498dff18decaf2fc0e5a5d7
ummahabibahabo <habiba.enh@gmail.com>
2026-09-12 23:54:32 +0600

    Navbar and Banner section complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Suspense } from "react";
 import "./App.css";
+import Banner from "./Components/Banner";
+import Navbar from "./Components/Navbar";
+import Technologies from "./Components/Technologies/Technologies";
 
 function App() {
   return (
-    <>
-      <h1>Hi i'm devStack</h1>
-    </>
+    <div className="container mx-auto px-20 py-10">
+      <Navbar></Navbar>
+      <Banner></Banner>
+      <Suspense fallback={<p>Loading.....</p>}>
+        <Technologies></Technologies>
+      </Suspense>
+    </div>
   );
 }
 
---
+++ src/Components/Technologies/Technologies.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Technologies = () => {
+  return (
+    <div>
+      <div className="space-y-3">
+        <h1 className="text-4xl text-[#0F172A] font-bold">
+          Explore the
+          <span className="text-[#EC4899]">Technologies</span>
+        </h1>
+        <p className="text-[16px]font-normal text-[#64748B]">
+          Pick one technology per category to build your ideal stack.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Technologies;
